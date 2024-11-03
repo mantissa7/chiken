ff1c7a98ae1f23a1e06caf0dfd6e1846b9e41851
Jordan Whitfield <jordan@zenbit.uk>
2024-11-03 14:07:34 +0000

    add top-level-await dep

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 })
